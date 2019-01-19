c2e34cd7f8d017bc2b803988765b4a0389708fe0
Salman Karim <Lego@MacBook-Pro-2.local>
2019-01-19 12:31:37 -0600

    update

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+
+
+    $("#submit").on("click", function(e) {
+       var employeeName = $("#employeeName").val();
+       console.log($('#employeeName').val);
+       var role = $($("#role")[0]).val()
+       var startDate = $("#startDate").val()
+       var monthlyRate = $("#monthlyRate").val()
+    
+       e.preventDefault()
+    
+       $("tbody").append(
+       `<tr>
+           <td>${employeeName}</td>
+           <td>${role}</td>
+           <td>${startDate}</td>
+           <td>72</td>
+           <td>${monthlyRate}</td>
+           <td>144000</td>
+       </tr>`)
+       $("#employeeName").val("");
+       $("#role").val("");
+       $("#startDate").val("");
+       $("#monthlyRate").val("");
+    });
+    });
