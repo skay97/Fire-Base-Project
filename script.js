fbca315116ccab7f4dc5b5b427049b4c7177808b
elhenderson <evann.henderson2@yahoo.com>
2019-01-19 13:19:27 -0600

    wtf is going on

--- script.js
+++ script.js
@@ -10,29 +10,62 @@ $(document).ready(function(){
   };
   firebase.initializeApp(config);
 
+  var database = firebase.database();
+
+
+  
 
 $("#submit").on("click", function(e) {
-    var employeeName = $("#employeeName").val();
-    console.log($('#employeeName').val);
-    var role = $($("#role")[0]).val()
-    var startDate = $("#startDate").val()
-    var monthlyRate = $("#monthlyRate").val()
+    
 
     e.preventDefault()
 
-    $("tbody").append(
-    `<tr>
-        <td>${employeeName}</td>
-        <td>${role}</td>
-        <td>${startDate}</td>
-        <td>72</td>
-        <td>${monthlyRate}</td>
-        <td>144000</td>
-    </tr>`)
+    var employeeName = $("#employeeName").val();
+    var role = $("#role").val();
+    var startDate = $("#startDate").val();
+    var monthlyRate = $("#monthlyRate").val();
+    
+
+    var newEmp = {
+        name: employeeName,
+        role: role,
+        start: startDate,
+        rate: monthlyRate
+    }
+
+    database.ref().push(newEmp);
+
+    
+
+
+
+
     $("#employeeName").val("");
     $("#role").val("");
     $("#startDate").val("");
     $("#monthlyRate").val("");
+
+
+});
+
+
+database.ref().on("child_added", function(childsnapshot) {
+    var employeeName = childsnapshot.val().name;
+    var role = childsnapshot.val().role;
+    var startDate = childsnapshot.val().start;
+    var monthlyRate = childsnapshot.val().rate;
+
+    $("tbody").append(
+        `<tr>
+            <td>${employeeName}</td>
+            <td>${role}</td>
+            <td>${startDate}</td>
+            <td>72</td>
+            <td>${monthlyRate}</td>
+            <td>144000</td>
+        </tr>`)
+
+
 });
 
 });
